f63d2dbd8c84a07cabfb90febf79e488dcdc2432
Michalik, Joshua <jsmichal@usa.wtbts.net>
2017-12-28 16:27:57 -0500

    Change answer sheet from markdown to html

--- generate-answer-sheet.js
+++ generate-answer-sheet.js
@@ -2,14 +2,17 @@ var fs = require('fs');
 var surveys = JSON.parse(fs.readFileSync('surveys.json', 'utf8'));
 var surveyNames = Object.keys(surveys);
 
-var answerSheet = "# Answer Sheet";
+var answerSheet = "<h1>Answer Sheet</h1>";
 
 surveyNames.forEach(function(key) {
-   answerSheet += "\n## " + key + "\n";
+   answerSheet += "<h2>" + key + "</h2>";
+   answerSheet += "<ol>";
    for (var i = 0; i < surveys[key].length; i++) {
       var answerNum = i + 1;
-      answerSheet += answerNum + ". " + surveys[key][i][0] + ": **" + surveys[key][i][1] + "**\n";
+      // answerSheet += answerNum + ". " + surveys[key][i][0] + ": **" + surveys[key][i][1] + "**\n";
+      answerSheet += "<li>" + surveys[key][i][0] + "<b> (" + surveys[key][i][1] + ")</b></li>";
    }
+   answerSheet += "</ol>";
 });
 
-fs.writeFileSync('answers.md', answerSheet);
+fs.writeFileSync('answers.html', answerSheet);

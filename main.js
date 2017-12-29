9e1e8fb76c0a9613edeee715966ada4f46e6b6e7
Michalik, Joshua <jsmichal@usa.wtbts.net>
2017-12-29 09:42:59 -0500

    Fix bug where board counts all flipped cards

--- main.js
+++ main.js
@@ -41,34 +41,34 @@ var app = {
       return array;
    },
    jsonLoaded: function (data) {
-      console.clear()
-      app.allData = data
-      app.questions = Object.keys(data)
-      app.shuffle(app.questions)
-      app.makeQuestion(app.currentQ)
-      $('body').append(app.board)
+      console.clear();
+      app.allData = data;
+      app.questions = Object.keys(data);
+      app.shuffle(app.questions);
+      app.makeQuestion(app.currentQ);
+      $('body').append(app.board);
    },
    // Action functions
    makeQuestion: function (qNum) {
-      var qText = app.questions[qNum]
-      var qAnswr = app.allData[qText]
+      var qText = app.questions[qNum];
+      var qAnswr = app.allData[qText];
 
-      var qNum = qAnswr.length
+      var qNum = qAnswr.length;
       qNum = (qNum < 8) ? 8 : qNum;
       qNum = (qNum % 2 != 0) ? qNum + 1 : qNum;
 
-      var boardScore = app.board.find("#boardScore")
-      var question = app.board.find(".question")
-      var col1 = app.board.find(".col1")
-      var col2 = app.board.find(".col2")
+      var boardScore = app.board.find("#boardScore");
+      var question = app.board.find(".question");
+      var col1 = app.board.find(".col1");
+      var col2 = app.board.find(".col2");
 
-      boardScore.html(0)
-      question.html(qText.replace(/&x22;/gi, '"'))
-      col1.empty()
-      col2.empty()
+      boardScore.html(0);
+      question.html(qText.replace(/&x22;/gi, '"'));
+      col1.empty();
+      col2.empty();
 
       for (var i = 0; i < qNum; i++) {
-         var aLI
+         var aLI;
          if (qAnswr[i]) {
             aLI = $("<div class='cardHolder'>" +
                "<div class='card'>" +
@@ -80,49 +80,43 @@ var app = {
                "<b class='LBG'>" + qAnswr[i][1] + "</b>" +
                "</div>" +
                "</div>" +
-               "</div>")
+               "</div>");
          } else {
-            aLI = $("<div class='cardHolder empty'><div></div></div>")
+            aLI = $("<div class='cardHolder empty'><div></div></div>");
          }
          var parentDiv = (i < (qNum / 2)) ? col1 : col2;
-         $(aLI).appendTo(parentDiv)
+         $(aLI).appendTo(parentDiv);
       }
 
-      var cardHolders = app.board.find('.cardHolder')
-      var cards = app.board.find('.card')
-      var backs = app.board.find('.back')
-      var cardSides = app.board.find('.card>div')
+      var cardHolders = app.board.find('.cardHolder');
+      var cards = app.board.find('.card');
+      var backs = app.board.find('.back');
+      var cardSides = app.board.find('.card>div');
 
       TweenLite.set(cardHolders, { perspective: 800 });
       TweenLite.set(cards, { transformStyle: "preserve-3d" });
       TweenLite.set(backs, { rotationX: 180 });
       TweenLite.set(cardSides, { backfaceVisibility: "hidden" });
 
-      cards.data("flipped", false)
+      cards.data("flipped", false);
 
       function showCard() {
-         var card = $('.card', this)
-         var flipped = $(card).data("flipped")
+         var card = $('.card', this);
+         var flipped = $(card).data("flipped");
          var cardRotate = (flipped) ? 0 : -180;
-         TweenLite.to(card, 1, { rotationX: cardRotate, ease: Back.easeOut })
-         flipped = !flipped
-         $(card).data("flipped", flipped)
-         app.getBoardScore()
+         TweenLite.to(card, 1, { rotationX: cardRotate, ease: Back.easeOut });
+         flipped = !flipped;
+         $(card).data("flipped", flipped);
+         app.getBoardScore(card);
       }
-      cardHolders.on('click', showCard)
+      cardHolders.on('click', showCard);
    },
-   getBoardScore: function () {
-      var cards = app.board.find('.card')
-      var boardScore = app.board.find('#boardScore')
-      var currentScore = { var: boardScore.html() }
-      var score = 0
-      function tallyScore() {
-         if ($(this).data("flipped")) {
-            var value = $(this).find("b").html()
-            score += parseInt(value)
-         }
-      }
-      $.each(cards, tallyScore)
+   getBoardScore: function (card) {
+      var cardScore = card.find("b").html();
+      var cards = app.board.find('.card');
+      var boardScore = app.board.find('#boardScore');
+      var currentScore = { var: boardScore.html() };
+      var score = parseInt(currentScore.var) + parseInt(cardScore);
       TweenMax.to(currentScore, 1, {
          var: score,
          onUpdate: function () {
@@ -132,12 +126,12 @@ var app = {
       });
    },
    awardPoints: function (num) {
-      var num = $(this).attr("data-team")
-      var boardScore = app.board.find('#boardScore')
-      var currentScore = { var: parseInt(boardScore.html()) }
-      var team = app.board.find("#team" + num)
-      var teamScore = { var: parseInt(team.html()) }
-      var teamScoreUpdated = (teamScore.var + currentScore.var)
+      var num = $(this).attr("data-team");
+      var boardScore = app.board.find('#boardScore');
+      var currentScore = { var: parseInt(boardScore.html()) };
+      var team = app.board.find("#team" + num);
+      var teamScore = { var: parseInt(team.html()) };
+      var teamScoreUpdated = (teamScore.var + currentScore.var);
       TweenMax.to(teamScore, 1, {
          var: teamScoreUpdated,
          onUpdate: function () {
@@ -155,15 +149,15 @@ var app = {
       });
    },
    changeQuestion: function () {
-      app.currentQ++
-      app.makeQuestion(app.currentQ)
+      app.currentQ++;
+      app.makeQuestion(app.currentQ);
    },
    // Inital function
    init: function () {
-      $.getJSON(app.jsonFile, app.jsonLoaded)
-      app.board.find('#newQuestion').on('click', app.changeQuestion)
-      app.board.find('#awardTeam1').on('click', app.awardPoints)
-      app.board.find('#awardTeam2').on('click', app.awardPoints)
+      $.getJSON(app.jsonFile, app.jsonLoaded);
+      app.board.find('#newQuestion').on('click', app.changeQuestion);
+      app.board.find('#awardTeam1').on('click', app.awardPoints);
+      app.board.find('#awardTeam2').on('click', app.awardPoints);
    }
 }
 app.init()
